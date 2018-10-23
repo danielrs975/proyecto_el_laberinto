b6ff1a2d7b58d127d2cc5f915e12d243bc2c39d0
DanielRS <danielrs9705@gmail.com>
2018-10-22 22:20:08 -0400

    Agrega que cada tipo de dato herede del tipo de dato Read

--- Laberinto.hs
+++ Laberinto.hs
@@ -1,12 +1,11 @@
 module Laberinto where
 import Data.Map as M
 
-data Laberinto = Laberinto Trifurcacion Tesoro deriving (Show)
+data Laberinto = Laberinto Trifurcacion Tesoro deriving (Show, Read)
 
 data Trifurcacion = Trifurcacion {  derecha :: Maybe Laberinto,
                                     izquierda :: Maybe Laberinto,
-                                    recto :: Maybe Laberinto} deriving (Show)
+                                    recto :: Maybe Laberinto} deriving (Show, Read)
 
 data Tesoro = Tesoro {  descripcion :: String,
-                        ignorar :: Maybe Laberinto} deriving (Show)
-
+                        ignorar :: Maybe Laberinto} deriving (Show, Read)
