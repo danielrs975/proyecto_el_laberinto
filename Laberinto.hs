f4aa8def0885be54df68d034179d191b2338d878
yuniquintero <yuni.quintero@gmail.com>
2018-10-22 23:21:17 -0400

    funciones de construccion

--- Laberinto.hs
+++ Laberinto.hs
@@ -10,3 +10,18 @@ data Trifurcacion = Trifurcacion {  derecha :: Maybe Laberinto,
 data Tesoro = Tesoro {  descripcion :: String,
                         ignorar :: Maybe Laberinto} deriving (Show)
 
+sin_salida :: Trifurcacion
+sin_salida = Trifurcacion{derecha=Nothing, izquierda=Nothing, recto=Nothing}
+
+desc_tesoro :: String -> Maybe Laberinto -> Tesoro
+desc_tesoro x y = Tesoro{descripcion=x, ignorar=y}
+
+cons_trif :: Trifurcacion -> Maybe Laberinto -> String -> Trifurcacion
+cons_trif x y z
+    | z=="derecha" = Trifurcacion{derecha=y, izquierda=izquierda x, recto=recto x}
+    | z=="izquierda" = Trifurcacion{derecha=derecha x, izquierda=y, recto=recto x}
+    | z=="recto" = Trifurcacion{derecha=derecha x, izquierda=izquierda x, recto=y}
+
+acc_izq :: Maybe Laberinto -> Maybe Laberinto
+acc_izq x = (Trifurcacion x) izquierda
+
