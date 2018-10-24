7113d7574e815af7c873aa597f284fe241568b4c
DanielRS <danielrs9705@gmail.com>
2018-10-23 21:58:43 -0400

    Se modifican las firmas de las funciones y se agregan Just a asignaciones

--- Laberinto.hs
+++ Laberinto.hs
@@ -9,7 +9,7 @@ import Data.Map as M
 -- Tipo de datos que almacena conocimientos del sabio sobre el laberinto
 -- Atributos: Trifurcacion y Tesoro
 data Laberinto = Laberinto  {trif :: Trifurcacion,
-                            tesoro :: Tesoro} deriving (Show)
+                             tesoro :: Maybe Tesoro} deriving (Show)
 
 -- Tipo de datos que almacena conocimientos del sabio sobre una trifurcacion
 -- del laberinto
@@ -42,24 +42,25 @@ sin_salida = Trifurcacion{derecha=Nothing, izquierda=Nothing, recto=Nothing}
 -- Parametros: x un String con la descripcion del tesoro, y un Maybe Laberinto
 -- que indica el laberinto a encontrar si se ignora el tesoro
 -- Salida: objeto Tesoro
-desc_tesoro :: String -> Maybe Laberinto -> Tesoro
-desc_tesoro x y = Tesoro{descripcion=x, ignorar=y}
+desc_tesoro :: String -> Laberinto -> Tesoro
+desc_tesoro x y = Tesoro{descripcion=x, ignorar=Just y}
+
 
 -- Funcion que recibe una Trifurcacion, un laberitno y un indicador de cual camino
 -- los relacion y retorna un Trifurcacion que indica que dicho camino conduce a 
 -- dicho laberitno
 -- Parametros: x una Trifurcacion, y un Maybe Laberinto, z un String que es nuestro
 -- indicador
-cons_trif :: Trifurcacion -> Maybe Laberinto -> String -> Trifurcacion
+cons_trif :: Trifurcacion -> Laberinto -> String -> Trifurcacion
 cons_trif x y z
-    | z=="derecha" = Trifurcacion{derecha=y, izquierda=izquierda x, recto=recto x}
-    | z=="izquierda" = Trifurcacion{derecha=derecha x, izquierda=y, recto=recto x}
-    | z=="recto" = Trifurcacion{derecha=derecha x, izquierda=izquierda x, recto=y}
+    | z=="derecha" = Trifurcacion{derecha=Just y, izquierda=izquierda x, recto=recto x}
+    | z=="izquierda" = Trifurcacion{derecha=derecha x, izquierda= Just y, recto=recto x}
+    | z=="recto" = Trifurcacion{derecha=derecha x, izquierda=izquierda x, recto=Just y}
 
 -- Funciones de Acceso
 
 -- Funcion que recibe un laberinto y retorna el laberinto que comienza al voltear
 -- a la izquierda
-acc_izq :: Maybe Laberinto -> Maybe Laberinto
-acc_izq x = (trif x) izquierda
+acc_izq :: Laberinto -> Maybe Laberinto
+acc_izq x =  izquierda $ trif x
 
