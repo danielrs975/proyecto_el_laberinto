ae667e867226c5ff8bd4e36650449e9d8c7e639e
yuniquintero <yuni.quintero@gmail.com>
2018-10-25 18:28:45 -0400

    se implememtan las funciones de acceso y de construccion

--- Laberinto.hs
+++ Laberinto.hs
@@ -8,102 +8,40 @@ import Data.Map as M
 import Data.List.Split
 import Data.Maybe
 
+data Laberinto = Tesoro String (Maybe Laberinto) | Trifurcacion (Maybe Laberinto) (Maybe Laberinto) (Maybe Laberinto) deriving (Show)
 
+instance Show Laberinto where
+    Show (Trifurcacion x y z) = show $ convert x 
 
--- Tipo de datos que almacena conocimientos del sabio sobre el laberinto
--- Atributos: Trifurcacion y Tesoro
-data Laberinto = Either Trifurcacion Tesoro deriving (Show)
--- Instancia de show para el tipo de dato Laberinto 
--- instance Show Laberinto where
-    -- show Laberinto {trif=x, tesoro=y} = "El laberinto es el siguiente: " ++ show x
+sin_salida :: Laberinto 
+sin_salida = Trifurcacion Nothing Nothing Nothing
 
--- Tipo de datos que almacena conocimientos del sabio sobre un tesoro
--- Atributos:
--- descripcion: un String con la descripcion del tesoro
--- ignorar: un Maybe Laberinto que indica que encontraran si se pasa por alto
--- al tesoro                                    
-data Tesoro = Tesoro {  descripcion :: String,
-                        ignorar :: Maybe Laberinto} deriving (Show)
+cons_tesoro :: String -> Laberinto -> Laberinto
+cons_tesoro x y = Tesoro x (Just y)
 
--- Tipo de datos que almacena conocimientos del sabio sobre una trifurcacion
--- del laberinto
--- Atributos:
--- derecha: un Maybe Laberinto que indica lo alcanzable al voltear a la derecha
--- izquierda: un Maybe Laberinto que indica lo alcanzable al voltear a la izquierda
--- recto: un Maybe Laberinto que indica lo alcanzable al seguir recto
-data Trifurcacion = Trifurcacion {  derecha :: Maybe Laberinto,
-                                    izquierda :: Maybe Laberinto,
-                                    recto :: Maybe Laberinto} deriving (Show)
+acc_izq :: Laberinto -> Maybe Laberinto
+acc_izq (Trifurcacion x _ _) = x
 
--- Funciones de Construccion
+acc_der :: Laberinto -> Maybe Laberinto
+acc_der (Trifurcacion _ x _) = x
 
--- Funcion que retorna un camino sin salida
--- Parametros: Void
--- Salida: Trifurcacion donde todos los caminos conducen a Nothing
-sin_salida :: Trifurcacion
-sin_salida = Trifurcacion{derecha=Nothing, izquierda=Nothing, recto=Nothing}
+acc_rec :: Laberinto -> Maybe Laberinto
+acc_rec (Trifurcacion _ _ x) = x
 
--- Funcion que recibe una descripcion de un tesoro y un laberinto indicando
--- que encontrarn si este se pasa por alto
--- Parametros: x un String con la descripcion del tesoro, y un Maybe Laberinto
--- que indica el laberinto a encontrar si se ignora el tesoro
--- Salida: objeto Tesoro
-desc_tesoro :: String -> Laberinto -> Tesoro
-desc_tesoro x y = Tesoro {descripcion=x, ignorar=Just y}
+conexion :: Laberinto -> Laberinto -> String -> Laberinto
+conexion x y z
+    |   z=="izquierda"  =  Trifurcacion (Just y) (acc_der x) (acc_rec x)
+    |   z=="derecha"    = Trifurcacion (acc_izq x) (Just y) (acc_rec x)
+    |   z=="recto"      = Trifurcacion (acc_izq x) (acc_der x) (Just y)
 
+convert :: Maybe a -> a
+convert x = case x of Nothing -> error "Se encontro una pared"
+                      Just x -> x
 
--- Funcion que recibe una Trifurcacion, un laberitno y un indicador de cual camino
--- los relacion y retorna un Trifurcacion que indica que dicho camino conduce a 
--- dicho laberitno
--- Parametros: x una Trifurcacion, y un Maybe Laberinto, z un String que es nuestro
--- indicador
-cons_trif :: Trifurcacion -> Laberinto -> String -> Trifurcacion
-cons_trif x y z
-    | z=="derecha" = Trifurcacion{derecha=Just y, izquierda=izquierda x, recto=recto x}
-    | z=="izquierda" = Trifurcacion{derecha=derecha x, izquierda= Just y, recto=recto x}
-    | z=="recto" = Trifurcacion{derecha=derecha x, izquierda=izquierda x, recto=Just y}
-
--- Funciones de Acceso
-
--- Funcion que recibe un laberinto y retorna el laberinto que comienza al voltear
--- a la izquierda
--- Parametros: x un Laberinto
--- Salida: El laberinto al voltear a la izquierda
-
--- Idea transformar lo que devuelve acceder en las direcciones a solo laberinto 
--- si encuentra pared devolver mensaje que diga encontro una pared
-acc_izq :: Laberinto -> Laberinto
-acc_izq x = case y of Nothing -> error "Se encontro una pared"
-                      Just y -> y 
-            where y = izquierda $ Left x
-
--- Funcion que recibe un laberinto y retorna el laberinto que comienza al voltear
--- a la derecha
--- Parametros: x un Laberinto
--- Salida: El laberinto al voltear a la derecha
-acc_der :: Laberinto -> Laberinto
-acc_der x  =  case y of Nothing -> error "Se encontro una pared"
-                       Just y -> y 
-             where y = derecha $ Left x
-
-
--- Funcion que recibe un laberinto y retorna el laberinto que comienza al seguir recto
--- Parametros: x un Laberinto
--- Salida: El laberinto al seguir recto
-acc_rec :: Laberinto -> Laberinto
-acc_rec x =  case y of Nothing -> error "Se encontro una pared"
-                       Just y -> y
-             where y = recto $ Left x
-
-get_ruta :: String -> [String]
-get_ruta x = splitOn " " x
-
-get_lab_head :: Laberinto -> [String] -> Laberinto
-get_lab_head x [] = x
-get_lab_head x (y:ys)
-    | y == "derecha" = get_lab_head (acc_der x) ys
-    | y == "izquierda" = get_lab_head (acc_izq x) ys
-    | y == "recto" = get_lab_head (acc_rec x) ys
-
-acc_ruta :: Laberinto -> String -> Laberinto
-acc_ruta x y = get_lab_head x $ get_ruta y
+recorrer :: Laberinto -> [String] -> Laberinto
+recorrer x [] = x
+recorrer x (y:ys)
+    |   y=="izquierda"  = recorrer (convert $ acc_izq x) ys
+    |   y=="derecha"  = recorrer (convert $ acc_der x) ys
+    |   y=="recto"  = recorrer (convert $ acc_rec x) ys
+            
